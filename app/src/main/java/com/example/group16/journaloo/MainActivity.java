6269a8ebfdc4c5ee11d85498bb4e8015bb152ad9
Toeter1996 <37299154+Toeter1996@users.noreply.github.com>
2018-03-14 11:41:20 +0100

    Added landing page

--- app/src/main/java/com/example/group16/journaloo/MainActivity.java
+++ app/src/main/java/com/example/group16/journaloo/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.group16.journaloo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // create custom toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.landing:
+                Toast.makeText(getApplicationContext(), "Landing", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.explore:
+                // fill in what should happen when clicked help
+                Toast.makeText(getApplicationContext(), "Explore", Toast.LENGTH_SHORT).show();
+                break; // break to end action only do the filled in actions
+            case R.id.history:
+                Toast.makeText(getApplicationContext(), "History", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.profile:
+                Toast.makeText(getApplicationContext(), "Profile", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
+    
 }
