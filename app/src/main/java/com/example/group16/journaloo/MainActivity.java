41356d17f94403281b0fd12d0d290a8051bd90c7
Toeter1996 <37299154+Toeter1996@users.noreply.github.com>
2018-03-21 11:58:04 +0100

    Camera
    method: storeCameraPhotoInSDCard, does not work yet. Error with fileOutputStream. (permission denied for some reason...)

--- app/src/main/java/com/example/group16/journaloo/MainActivity.java
+++ app/src/main/java/com/example/group16/journaloo/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.group16.journaloo;
 
+
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -8,13 +14,23 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
     public boolean journeyActive = false; // becomes true if journey is saved, is used to change layout of MainActivity
-
+    static final int requestCode =20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
             setContentView(R.layout.activity_main_2);
             TextView nameJourney = (TextView) findViewById(R.id.nameJourney);
             nameJourney.setText(getIntent().getExtras().getString("nameJourney"));
+
         }
 
 
@@ -39,6 +56,8 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
     }
+
+
 
 
     public void stopJourney(View view){
@@ -53,6 +72,83 @@ public class MainActivity extends AppCompatActivity {
         finish();
         startActivity(intent);
     }
+
+
+    public void openCamera(View view) {
+        Intent photoCaptureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        String state = Environment.getExternalStorageState();
+        if(Environment.MEDIA_MOUNTED.equals(state)){
+            Toast.makeText(getApplicationContext(),"SD card found",Toast.LENGTH_LONG).show();
+        } else{
+            Toast.makeText(getApplicationContext(),"SD card not found",Toast.LENGTH_LONG).show();
+        }
+
+
+
+
+        startActivityForResult(photoCaptureIntent, requestCode);
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if(MainActivity.requestCode == requestCode && resultCode == RESULT_OK){
+
+            ImageView entryView = (ImageView) findViewById(R.id.entryView);
+            Bundle extras = data.getExtras();
+            Bitmap bitmap = (Bitmap) extras.get("data");
+            entryView.setImageBitmap(bitmap);
+
+
+            String partFilename = currentDateFormat();
+            storeCameraPhotoInSDCard(bitmap, partFilename);
+
+            // display the image from SD Card to ImageView Control
+            //String storeFilename = "photo_" + partFilename + ".jpg";
+           // Bitmap mBitmap = getImageFileFromSDCard(storeFilename);
+           // entryView.setImageBitmap(mBitmap);
+        }
+
+    }
+
+    private String currentDateFormat(){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HH_mm_ss");
+        String  currentTimeStamp = dateFormat.format(new Date());
+        return currentTimeStamp;
+    }
+
+    private void storeCameraPhotoInSDCard(Bitmap bitmap, String currentDate){
+        File outputFile = new File(Environment.getExternalStorageDirectory(), "photo_" + currentDate + ".jpg");
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(outputFile);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream);
+            fileOutputStream.flush();
+            fileOutputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Bitmap getImageFileFromSDCard(String filename){
+        Bitmap bitmap = null;
+        File imageFile = new File(Environment.getExternalStorageDirectory() + filename);
+        try {
+            FileInputStream fis = new FileInputStream(imageFile);
+            bitmap = BitmapFactory.decodeStream(fis);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
