3be22b1899dea67ba236c9be88a5dc90e83af8d3
Chantal <chantalvdluijtgaarden@live.nl>
2018-03-23 14:16:30 +0100

    save new entry features

--- app/src/main/java/com/example/group16/journaloo/NewEntryActivity.java
+++ app/src/main/java/com/example/group16/journaloo/NewEntryActivity.java
@@ -1,11 +1,15 @@
 package com.example.group16.journaloo;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.FileInputStream;
 
@@ -31,11 +35,22 @@ public class NewEntryActivity extends AppCompatActivity {
         entryImageView.setImageBitmap(bmp);
         //entryView.setImageDrawable(Drawable.createFromPath(path));
 
-
-
-
-
     }
 
-
+    public void saveEntry(View view) {
+        EditText descriptionEntry = (EditText) findViewById(R.id.descriptionEntryEditText);
+        EditText locationEntry = (EditText) findViewById(R.id.locationEntryEditText);
+        if (locationEntry.getText().toString().matches("")){
+            Toast.makeText(getApplicationContext(), "Please give the entry a location", Toast.LENGTH_SHORT).show();
+        } else if (descriptionEntry.getText().toString().matches("")){
+            Toast.makeText(getApplicationContext(), "Please give the entry a description", Toast.LENGTH_SHORT).show();
+        } else {
+            nameJourney = editJourneyName.getText().toString();
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.putExtra("nameJourney", editJourneyName.getText().toString());
+            intent.putExtra("isActive", journeyActive);
+            finish();
+            startActivity(intent);
+        }
+    }
 }
