ff15811e6b468092ae0e3a0e18feb9855f13ea00
SannevanDalen <37299519+SannevanDalen@users.noreply.github.com>
2018-03-28 12:16:09 +0200

    als er geen journey is krijg je een lege pagina

--- app/src/main/java/com/example/group16/journaloo/ViewJourneyActivity.java
+++ app/src/main/java/com/example/group16/journaloo/ViewJourneyActivity.java
@@ -21,24 +21,28 @@ public class ViewJourneyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_journey);
         lst =(ListView)findViewById(R.id.listview);
-        String[] journeyNames = new String[journeys.length];
-        for (int i = 0; i < journeys.length; i++) {
-            Journey journey = journeys[i];
-            String titleJ = journey.title;
-            journeyNames[i] = titleJ;
-        }
-        CustomListview customListview=new CustomListview(this, journeyNames);
-        lst.setAdapter(customListview);
-        for (Journey journey: journeys){
-            final Integer journeyid = journey.journeyId;
-            lst.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent intent = new Intent(ViewJourneyActivity.this, ViewEntries.class);
-                intent.putExtra("JourneyId", journeyid);
-                startActivity(intent);
+        if (journeys == null) {
+
+        } else {
+            String[] journeyNames = new String[journeys.length];
+            for (int i = 0; i < journeys.length; i++) {
+                Journey journey = journeys[i];
+                String titleJ = journey.title;
+                journeyNames[i] = titleJ;
             }
-            });
+            CustomListview customListview = new CustomListview(this, journeyNames);
+            lst.setAdapter(customListview);
+            for (Journey journey : journeys) {
+                final Integer journeyid = journey.journeyId;
+                lst.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        Intent intent = new Intent(ViewJourneyActivity.this, ViewEntries.class);
+                        intent.putExtra("JourneyId", journeyid);
+                        startActivity(intent);
+                    }
+                });
+            }
         }
     }
 
