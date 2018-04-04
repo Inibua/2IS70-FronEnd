955ad3d85973ee521bc60ba06d4bcff7a932a342
frank <frank.049@hotmail.com>
2018-04-04 11:45:34 +0200

    Add location and date to entry cards

--- app/src/main/java/com/example/group16/journaloo/activity/EntryCardAdapter.java
+++ app/src/main/java/com/example/group16/journaloo/activity/EntryCardAdapter.java
@@ -1,24 +1,28 @@
 package com.example.group16.journaloo.activity;
 
 
-import android.support.v7.widget.CardView;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.example.group16.journaloo.R;
-import com.example.group16.journaloo.api.APIWrapper;
 import com.example.group16.journaloo.api.GlideApp;
 import com.example.group16.journaloo.model.Entry;
-import okhttp3.HttpUrl;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class EntryCardAdapter extends RecyclerView.Adapter<EntryCardAdapter.ViewHolder> {
     private List<Entry> entryList;
-    private APIWrapper wrapper;
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
@@ -34,6 +38,8 @@ public class EntryCardAdapter extends RecyclerView.Adapter<EntryCardAdapter.View
         public ViewHolder(View v) {
             super(v);
             cardView = v;
+            location = v.findViewById(R.id.locationTextView);
+            date =  v.findViewById(R.id.dateTextView);
             description = v.findViewById(R.id.descriptionTextView);
             image = v.findViewById(R.id.entryImageView);
         }
@@ -42,12 +48,11 @@ public class EntryCardAdapter extends RecyclerView.Adapter<EntryCardAdapter.View
     // Provide a suitable constructor (depends on the kind of dataset)
     public EntryCardAdapter(List<Entry> entryList) {
         this.entryList = entryList;
-        this.wrapper = APIWrapper.getWrapper();
     }
 
     // Create new views (invoked by the layout manager)
     @Override
-    public EntryCardAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+    public EntryCardAdapter.ViewHolder onCreateViewHolder(final ViewGroup parent,
                                                           int viewType) {
         // create a new view
         View v = LayoutInflater.from(parent.getContext())
@@ -58,8 +63,23 @@ public class EntryCardAdapter extends RecyclerView.Adapter<EntryCardAdapter.View
 
     // Replace the contents of a view (invoked by the layout manager)
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        Entry entry = entryList.get(position);
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        final Entry entry = entryList.get(position);
+
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = holder.cardView.getContext();
+                Intent intent = new Intent(context, EditEntryActivity.class);
+                Log.i("EntryCard", "Hi you clicked a button amigo");
+                intent.putExtra("id", entry.id);
+                intent.putExtra("location", entry.location);
+                intent.putExtra("description", entry.description);
+
+                context.startActivity(intent);
+            }
+        });
+
         String url = "https://polar-cove-19347.herokuapp.com/entry/" + entry.id + "/image";
 
         GlideApp.with(holder.image.getContext())
@@ -67,6 +87,16 @@ public class EntryCardAdapter extends RecyclerView.Adapter<EntryCardAdapter.View
                 .centerCrop()
                 .into(holder.image);
 
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSS", Locale.ENGLISH);
+//        format.setTimeZone(TimeZone.getTimeZone("America/New_York"));
+        try {
+            Date date = format.parse(entry.created);
+            holder.date.setText(date.toString());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        holder.location.setText(entry.location);
         holder.description.setText(entry.description);
     }
 
